61e3be53be8a438654c9b0c8d97caa0948dec829
silvajhb <jhbs.student@gmail.com>
2022-05-20 16:04:24 -0300

    DESAFIO 008 CONCLUÍDO

--- DESAFIOS/DESAFIO_007.py
+++ DESAFIOS/DESAFIO_007.py
@@ -3,6 +3,6 @@
 print(f"{'DESAFIO 007':=^20}")
 
 n1 = int(input("Digite a primeira Nota: "))
-n2 = int(input("digite a Segunda Nota": ))
+n2 = int(input("digite a Segunda Nota:" ))
 
 print(f"A média das notas é: {(n1 + n2) / 2}")
--- DESAFIOS/DESAFIO_008.py
+++ DESAFIOS/DESAFIO_008.py
@@ -1,1 +1,9 @@
 # Escrava um programa que leia um valor em metros e o exiba convertido am centímetros e milímetros.
+
+
+print(f"{'DESAFIO 008':=^20}")
+
+m = float(input("Digite uma medida em metros: "))
+
+print(f"A Medida em Centímetros é: {m * 100}")
+print(f"A Medida em Milímetros é: {m * 1000}")
