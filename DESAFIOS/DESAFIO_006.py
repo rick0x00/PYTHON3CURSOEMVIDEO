5cb9639238a56da930c698839af24fa7910eaaf4
silvajhb <jhbs.student@gmail.com>
2022-05-20 15:43:08 -0300

    desafio 006 concluído

--- DESAFIOS/DESAFIO_006.py
+++ DESAFIOS/DESAFIO_006.py
@@ -1,1 +1,9 @@
 # Cria um algoritmo que leia um número e mostre o seu dobro, triplo a raiz quadrada.
+
+print(f"{'DESAFIO 006':=^20}")
+
+n = int(input("Digite um número: "))
+
+print(f"O dobro do número é: {n * 2}")
+print(f"O triplo do número é: {n * 3}")
+print(f"A Raiz quadrada é: {int(n ** (1/2))}")
