d694c61accb908882657586fa8b28fae7b15ff00
silvajhb <jhbs.student@gmail.com>
2022-05-20 15:51:34 -0300

    DESAFIO 008 CONCLUÍDO

--- DESAFIOS/DESAFIO_007.py
+++ DESAFIOS/DESAFIO_007.py
@@ -1,1 +1,8 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule a mostre a sua média.
+
+print(f"{'DESAFIO 007':=^20}")
+
+n1 = int(input("Digite a primeira Nota: "))
+n2 = int(input("digite a Segunda Nota": ))
+
+print(f"A média das notas é: {(n1 + n2) / 2}")
