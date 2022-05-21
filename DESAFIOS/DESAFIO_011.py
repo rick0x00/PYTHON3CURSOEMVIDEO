d60eaa853815df8223acd2f1801f6bd2750706e0
silvajhb <jhbs.student@gmail.com>
2022-05-20 23:48:59 -0300

    DESAFIO 11 CONCLUÍDO

--- DESAFIOS/DESAFIO_011.py
+++ DESAFIOS/DESAFIO_011.py
@@ -1,1 +1,8 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcula a Sua área R a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m².
+
+print(f"{'DESAFIO 011':=^20}")
+
+L = float(input("Informe a Largura da Parede: "))
+H = float(input("Informe a Altura da Parede: "))
+
+print(f"Você precisa de {(L * H)/2} Litros de tinta!")
