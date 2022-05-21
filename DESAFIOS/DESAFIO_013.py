0e1efb230dcb5952c869048b8b4c0661c3d563ab
silvajhb <jhbs.student@gmail.com>
2022-05-21 00:21:54 -0300

    DESAFIO 13 CONCLUÍDO

--- DESAFIOS/DESAFIO_013.py
+++ DESAFIOS/DESAFIO_013.py
@@ -1,1 +1,7 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+print(f"{'DESAFIO 013':=^20}")
+
+salario = float(input("Informe seu salário: "))
+
+print(f'O seu salário com aumento de 15% será de: R$: {salario * 1.15}')
