c8449a1a0165e957c0e570df879995e7298dcdcc
silvajhb <jhbs.student@gmail.com>
2022-05-20 23:58:16 -0300

    DESAFIO 012 CONCLUÍDO

--- DESAFIOS/DESAFIO_012.py
+++ DESAFIOS/DESAFIO_012.py
@@ -1,1 +1,7 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preso, com 5% de desconto.
+
+print(f"{'DESAFIO 012':=^20}")
+
+valor = float(input("Informe o valor do produto: "))
+
+print(f"Com 5% de desconto o produto fica por: R$:{valor*0.95} !")
