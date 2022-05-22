38868200f1484a9142578240bb1b0cf98843e90c
silvajhb <jhbs.student@gmail.com>
2022-05-22 17:41:27 -0300

    ajustes do desafio 014

--- DESAFIOS/DESAFIO_014.py
+++ DESAFIOS/DESAFIO_014.py
@@ -4,4 +4,4 @@ print(f"{'DESAFIO 014':=^25} \n"+"Conversor de Temperaturas")
 
 TEMP_C = float(input("Infome a temperatura em °C: "))
 
-print(f"A temperatura de {TEMP_C}°C corresponde a {((TEMP_C*(9/5))+32):.2f}°F !")
+print(f"A temperatura de {TEMP_C}°C corresponde a {( (TEMP_C * ( 9 / 5 )) + 32 ):.2f}°F !")
