657ac465e9b3d42c402c5a1df710b259e24304e1
silvajhb <jhbs.student@gmail.com>
2022-05-22 13:05:51 -0300

    AJUSTE DO DESAFIO 007

--- DESAFIOS/DESAFIO_007.py
+++ DESAFIOS/DESAFIO_007.py
@@ -2,7 +2,7 @@
 
 print(f"{'DESAFIO 007':=^20}")
 
-n1 = int(input("Digite a primeira Nota: "))
-n2 = int(input("digite a Segunda Nota:" ))
+n1 = float(input("Digite a primeira Nota: "))
+n2 = float(input("digite a Segunda Nota:" ))
 
-print(f"A média das notas é: {(n1 + n2) / 2}")
+print(f"A média das notas é: {((n1 + n2) / 2):.2f}")
