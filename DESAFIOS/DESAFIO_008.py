6f682b93d89a189b289524e7227bff7dcbe3a3a2
silvajhb <jhbs.student@gmail.com>
2022-05-22 13:30:28 -0300

    AJUSTE DO DESAFIO 008

--- DESAFIOS/DESAFIO_008.py
+++ DESAFIOS/DESAFIO_008.py
@@ -7,3 +7,11 @@ m = float(input("Digite uma medida em metros: "))
 
 print(f"A Medida em Centímetros é: {m * 100}")
 print(f"A Medida em Milímetros é: {m * 1000}")
+
+
+# Sujestões de complemento sujeridas em video
+
+print(f"A Medica em Km é: {m / 1000}")
+print(f"A medida em Hm é: {m / 100}")
+print(f"A medida em Dam é: {m / 10}")
+print(f"A Medida em dm é: {m * 10}")
