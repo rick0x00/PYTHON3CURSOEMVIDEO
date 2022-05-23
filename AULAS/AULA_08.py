531ac696f0ee3d173c1459fff3eea501a6ee5394
silvajhb <jhbs.student@gmail.com>
2022-05-22 22:23:38 -0300

    AULA 08 CONCLUÍDA

--- AULAS/AULA_08.py
+++ AULAS/AULA_08.py
@@ -1,3 +1,23 @@
 # Trabalhando com Módulos
 
 # módulos são bibliotecas que podem ser incluídas no algoritmo
+
+# a biblioteca "math" inclui as funções: ceil, floor, trunc, pow, sqrt, factorial ...
+
+# é possível importar a biblioteca inteira
+
+import math
+
+# é possível importar uma única função da biblioteca
+
+from math import sqrt
+
+# ou podemos importar mais de uma função
+
+from math import sqrt, trunc
+
+num = int(input("Digite um Numero: "))
+
+raiz = math.sqrt(num)
+
+print(f"A raiz de {num} é igual a {(raiz):.2f}")
