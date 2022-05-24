9f156fd392f403519982db890c9391dccd7722ac
silvajhb <jhbs.student@gmail.com>
2022-05-24 18:04:38 -0300

    DESAFIO 16 CONCLUÍDO

--- DESAFIOS/DESAFIO_016.py
+++ DESAFIOS/DESAFIO_016.py
@@ -1,1 +1,10 @@
 # Crie um programa que leia um numero Real qualquer pelo teclado e mostre na tela a sua porção Inteira
+
+print(f"{'DESAFIO 16':=^20}")
+print("Quebrando um Número")
+
+import math
+
+n = float(input("Informe um número: "))
+
+print(f"A porção inteira do numero é: {math.floor(n)}")
