aad4a39b6befde93133c108792b515ba11821958
silvajhb <jhbs.student@gmail.com>
2022-05-25 15:46:14 -0300

    inicio do desafio 18

--- DESAFIOS/DESAFIO_018.py
+++ DESAFIOS/DESAFIO_018.py
@@ -1,1 +1,8 @@
 # Faça um Programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse angulo.
+
+print(f"{'DESAFIO 18':=^20}")
+print("Label")
+
+angle = float(input("Informe o valor de um Angulo em Graus: "))
+
+print(f"Segue Informações sobre o Angulo {angle}°")
