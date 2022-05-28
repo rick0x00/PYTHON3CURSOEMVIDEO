7c5c0d6af9ceb21ec17463e23774424acc2b8e72
silvajhb <jhbs.student@gmail.com>
2022-05-28 10:15:23 -0300

    desafio 022 concluído

--- DESAFIOS/DESAFIO_022.py
+++ DESAFIOS/DESAFIO_022.py
@@ -3,3 +3,14 @@
 # O nome com todas minúsculas.
 # Quantas letras ao todo (sem considerar Espaços).
 # Quantas letras tem o primeiro nome.
+
+print(f"{'DESFIO':=^20}")
+print("Analisador de Textos")
+
+nome = str(input("Informe seu nome completo..."))
+
+print("Analisando seu nome completo")
+print(f"Seu nome em Maiúsculo é: {nome.upper()}")
+print(f"Seu nome em Minusculo é: {nome.lower()}")
+print(f"Seu nome tem {len(''.join(nome.split()))} letras")
+print(f"Seu primeiro nome tem {len(nome.split()[0])} letras")
