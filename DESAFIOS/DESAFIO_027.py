323fec566682a51c1ccda6a008a2e0391788db4f
silvajhb <jhbs.student@gmail.com>
2022-05-29 20:48:59 -0300

    DESAFIO 027 CONCLUÍDO

--- DESAFIOS/DESAFIO_027.py
+++ DESAFIOS/DESAFIO_027.py
@@ -2,3 +2,16 @@
 # Ex: Ana Maria de Souza 
 # primeiro = Ana 
 # último=Souza
+
+N_desafio = "027"
+nome_desafio = "Primeiro e último nome de uma pessoa"
+print(f'{" DESAFIO"f" {N_desafio} ":=^{len(nome_desafio)}}')
+print(nome_desafio)
+print(f'{"":=^{len(nome_desafio)}}')
+
+nome_pessoa = str(input("Informe seu nome completo: ")).strip()
+
+print(f"Muito prazer em te conhecer {nome_pessoa}!")
+print(f"Seu primeiro nome é {nome_pessoa.split()[0]}")
+# print(f"Seu ultimo nome é {nome_pessoa.split()[len(nome_pessoa.split())-1]}")
+print(f"Seu ultimo nome é {nome_pessoa.split()[-1]}")
