9a4d8437314ed01ed51e3a5d3d859856e00c113c
silvajhb <jhbs.student@gmail.com>
2022-05-29 12:03:42 -0300

    arquivo modelo de deszafio criado

---
+++ DESAFIOS/DESAFIO_000.py
@@ -0,0 +1,7 @@
+# descrição do desafio
+
+N_desafio = "numero do desafio"
+nome_desafio = "nome do desafio"
+print(f'{" DESAFIO"f" {N_desafio} ":=^{len(nome_desafio)}}')
+print(nome_desafio)
+print(f'{"":=^{len(nome_desafio)}}')
