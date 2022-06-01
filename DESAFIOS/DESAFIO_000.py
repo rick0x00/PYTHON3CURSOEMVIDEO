50cb6036430df9be2dc330fd07968c333779145f
silvajhb <jhbs.student@gmail.com>
2022-06-01 16:41:05 -0300

    adjust padrão de desafio

--- DESAFIOS/DESAFIO_000.py
+++ DESAFIOS/DESAFIO_000.py
@@ -4,4 +4,9 @@ N_desafio = "numero do desafio"
 nome_desafio = "nome do desafio"
 print(f'{" DESAFIO"f" {N_desafio} ":=^{len(nome_desafio)}}')
 print(nome_desafio)
-print(f'{"":=^{len(nome_desafio)}}')
+print('=' * len(nome_desafio))
+
+msg_desafio = str("")
+print('-' * int(len(msg_desafio)))
+print(msg_desafio)
+print('-' * int(len(msg_desafio)))
