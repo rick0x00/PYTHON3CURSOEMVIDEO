fd98e33d0657eb5bad12f3db3380204d6fb4feb7
silvajhb <jhbs.student@gmail.com>
2022-06-01 16:53:49 -0300

    desafio 029 concluído

--- DESAFIOS/DESAFIO_029.py
+++ DESAFIOS/DESAFIO_029.py
@@ -3,8 +3,20 @@
 # Se ale ultrapassar 80Km/h. mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$7.00 por cada Km acima do limita.
 
-N_desafio = "numero do desafio"
-nome_desafio = "nome do desafio"
+N_desafio = "029"
+nome_desafio = "Radar eletrônico"
 print(f'{" DESAFIO"f" {N_desafio} ":=^{len(nome_desafio)}}')
 print(nome_desafio)
-print(f'{"":=^{len(nome_desafio)}}')
+print('=' * len(nome_desafio))
+
+msg_desafio = str(""" Escrava um programa que leia a velocidade de um carro. Se ale ultrapassar 80Km/h. mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7.00 por cada Km acima do limita.""")
+print('-' * int(len(msg_desafio)))
+print(msg_desafio)
+print('-' * int(len(msg_desafio)))
+
+velocidade_carro = float(input("Informe a velocidade do carro! "))
+
+if velocidade_carro > 80 :
+    print("Você foi multado por ultrapassar a velocidade limite de 80Km/h")
+else :
+    print("Você não ultrapassou a velocidade limite de 80Km/h")
