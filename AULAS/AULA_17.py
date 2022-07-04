2c8c21c8b543e4727f8835c34d141b6255fbef3f
Henrique Silva <rick.0x00@gmail.com>
2022-07-04 20:05:03 -0300

    AULA 17 CONCLUÍDA

--- AULAS/AULA_17.py
+++ AULAS/AULA_17.py
@@ -51,4 +51,47 @@ valores = [4, 8, 2, 5, 6]
 # ordenando valores
 print(sorted(valores))
 # ordem reversa
-print(valores.sort(reverse=True))
+valores.sort(reverse=True)
+print(valores)
+
+# o ato de copiar uma lista cria links entre elas
+a = [2, 4, 8, 7]
+b = a
+b[2] = 5
+print(f"Lista A: {a}")
+print(f"Lista B: {b}")
+
+# assim você consegue de fato copiar os valores sem criar links entre elas
+c = [9, 3, 6, 1]
+d = c[:]
+d[2] = 5
+print(f"Lista A: {c}")
+print(f"Lista B: {d}")
+
+# "COM OS CONHECIMENTOS DESSA AULA, ESTÁ PASSÍVEL DE RESOLUÇÃO OS DESAFIOS:"
+
+# Considerando os Conhecimentos da Aula 17
+
+# DESAFIO 078 - Maior e Menor valores na Lista
+# Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista. 
+
+# DESAFIO 079 - Valores únicos em uma Lista
+# Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente. 
+
+# DESAFIO 080 - Lista ordenada sem repetições
+# Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela
+
+# DESAFIO 081 - Extraindo dados de uma Lista
+# Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+# A) Quantos números foram digitados.
+# B) A lista de valores, ordenada de forma decrescente.
+# C) Se o valor 5 foi digitado e está ou não na lista.
+
+# DESAFIO 082 - Dividindo valores em várias listas
+# Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+
+# DESAFIO 083 - Validando expressões matemáticas
+# Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+
+
