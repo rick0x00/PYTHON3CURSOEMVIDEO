b00e7d6e92bd579df9bc1cfe60a6ac18c3b1be64
Henrique Silva <rick.0x00@gmail.com>
2022-07-08 22:46:00 -0300

    desafio 081 Concluído

--- DESAFIOS/DESAFIO_081.py
+++ DESAFIOS/DESAFIO_081.py
@@ -10,3 +10,30 @@ nome_desafio = "Extraindo dados de uma Lista"
 print(f'{" DESAFIO"f" {N_desafio} ":=^{len(nome_desafio)}}')
 print(nome_desafio)
 print('=' * len(nome_desafio))
+
+lista = []
+opc = 'Ss'
+while opc not in 'Nn' :
+    while True :
+        num = (input("Informe um número: ")).strip()
+        if num.isnumeric() == 0 :
+            print("Valor invalido")
+        else :
+            lista.append(int(num))
+            break
+    while True :
+        opc = (input("Quer Continuar: [S/N] ")).strip()
+        if opc.isalpha() == 1 :
+            if opc in 'SsNn' :
+                break
+            else:
+                print("Valor invalido")
+        else :
+            print("Valor invalido")
+
+
+print("existem", len(lista), "Valores na Lista")
+lista.sort(reverse=True)
+print("A lista em ordem reversa é: ",lista)
+if 5 in lista:
+    print("O valor 5 está na lista")
