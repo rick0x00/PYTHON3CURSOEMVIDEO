f253c0e69551ffc889ccf5a173e85c54c6b38e2a
Henrique Silva <rick.0x00@gmail.com>
2022-07-12 20:55:39 -0300

    aula 18 pausada

--- AULAS/AULA_18.py
+++ AULAS/AULA_18.py
@@ -2,3 +2,55 @@
 # VARIÁVEIS COMPOSTAS - LISTAS - PARTE 2
 
 # LISTAS COMPOSTAS, LISTAS DENTRO DE LISTAS
+
+# voce pode cira uma variável definindo sua classe ou com colchetes fechados vazios
+dados = list()
+dados.append("Henrique")
+dados.append("21")
+
+print(dados[0])
+print(dados[1])
+
+pessoas = list()
+
+# colocando listas dentro de outras listas
+pessoas.append(dados[:])
+# abaixo percebe que existe diferença em uma temos uma lista dentro de uma lista e na outra temos uma variável simples dentro de uma lista
+pessoas.append(['teste11'])
+pessoas.append('teste12')
+# adicionando uma lista dentro de outra lista de forma mais manual
+pessoas.append(["teste21","teste22"]) 
+
+# observe os retornos
+print(pessoas)
+print(pessoas[0])
+print(pessoas[1])
+print(pessoas[2])
+
+# com dois indices podemos extrair informações especificas de um índice dentro de outro índice
+print(pessoas[0][0])
+print(pessoas[0][1])
+
+# outra forma de criar listas compostas
+galera = [['josé', 20], ['henrique', 21], ['barbosa', 22], ['silva', 23]]
+
+print(galera)
+print(galera[2])
+print(galera[2][1])
+print(galera[3][0])
+
+for p in galera :
+    print(f"{p[0]} tem {p[1]} anos de idade")
+
+################
+
+dado = list()
+info = list()
+
+for c in range(0, 3) :
+    dado.append(str(input("Informe seu nome: ")))
+    dado.append(int(input("Informe sua idade: ")))
+    info.append(dado[:])
+    dado.clear()
+
+print(info)
