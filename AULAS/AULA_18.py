7184bace77fc81f247822bced8573d2229cdd7a3
Henrique Silva <rick.0x00@gmail.com>
2022-07-14 21:59:38 -0300

    DESAFIOS ATRIBUIDOS

--- AULAS/AULA_18.py
+++ AULAS/AULA_18.py
@@ -65,3 +65,33 @@ for c in info :
 
 print(f"temos {totmai} maiores de idade, e temos {totmen} menores de idade")
 print(info)
+
+# "COM OS CONHECIMENTOS DESSA AULA, ESTÁ PASSÍVEL DE RESOLUÇÃO OS DESAFIOS: 084, 085, 086, 087, 088, 089"
+
+# Considerando os Conhecimentos da Aula 18
+
+# DESAFIO 084 - Lista composta e análise de dados
+# Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas.
+# B) Uma listagem com as pessoas mais pesadas.
+# C) Uma listagem com as pessoas mais leves.
+
+# DESAFIO 085 - Listas com pares e ímpares
+# Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+
+# DESAFIO 086 - Matriz em Python
+#  Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
+
+# DESAFIO 087 - Mais sobre Matriz em Python
+# Aprimore o desafio anterior, mostrando no final: 
+# A) A soma de todos os valores pares digitados.
+# B) A soma dos valores da terceira coluna.
+# C) O maior valor da segunda linha.
+
+# DESAFIO 088 - Palpites para a Mega Sena
+# Faça um programa que ajude um jogador da MEGA SENA a criar palpites.O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
+
+# DESAFIO 089 - Boletim com listas compostas
+# Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
+
