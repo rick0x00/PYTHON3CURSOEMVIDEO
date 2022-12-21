0bc7174ee99c522bb589d402185761d0a78c4754
rick0x00 <rick.0x00@gmail.com>
2022-12-20 22:36:12 -0300

    desafios da aula 21 atribuídos

--- AULAS/AULA_21.py
+++ AULAS/AULA_21.py
@@ -75,3 +75,33 @@ def par(a=0):
 print(par(5))
 
 print(par(8))
+
+
+# "COM OS CONHECIMENTOS DESSA AULA, ESTÁ PASSÍVEL DE RESOLUÇÃO OS DESAFIOS: 101, 102, 103, 104, 105, 106"
+
+# Considerando os Conhecimentos da Aula 21
+
+
+# DESAFIO 101 - Funções para votação
+# Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
+
+# DESAFIO 102 - Função para Fatorial
+# Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+# DESAFIO 103 -  Ficha do Jogador
+# Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+# DESAFIO 104 - Validando entrada de dados em Python
+# Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
+# Ex: n = leiaInt('Digite um n: ')
+
+# DESAFIO 105 - Analisando e gerando Dicionários
+# Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+#- Quantidade de notas
+#- A maior nota
+#- A menor nota
+#- A média da turma
+#- A situação (opcional)
+
+# DESAFIO 106 -  Sistema interativo de ajuda em Python
+# Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o programa se encerrará. Importante: use cores.
